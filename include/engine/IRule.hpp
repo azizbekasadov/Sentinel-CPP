2ec7ecfbefaffc08c8926757855077d5108faf8d
Azizbek Asadov <azizbek.asadov.work@gmail.com>
2026-04-08 01:49:02 +0200

    Add Rule Match business logic templates for files and string matching policy in the scanner

---
+++ include/engine/IRule.hpp
@@ -0,0 +1,52 @@
+//
+//  IRule.hpp
+//  Sentinel-CPP
+//
+//  Created by Azizbek Asadov on 08.04.2026.
+//
+//
+
+
+#ifndef SCANNER_HPP
+#define SCANNER_HPP
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <string_view>
+
+using namespace std;
+
+namespace sentinel::engine {
+
+struct RuleMatch {
+    string rule_id;
+    string description;
+    size_t offset;
+    
+    bool operator==(const RuleMatch &) const = default;
+};
+
+class IRule {
+public:
+    virtual ~IRule() = default;
+    
+    IRule(const IRule&)              = delete;
+    IRule& operator=(const IRule&)   = delete;
+    IRule(IRule&&)                   = delete;
+    IRule& operator=(IRule&&)       = delete;
+    
+    [[nodiscard]] virtual vector<RuleMatch> apply(string_view data) const = 0;
+    [[nodiscard]] virtual string_view id() const = 0;
+    [[nodiscard]] virtual string_view description() const = 0;
+    
+protected:
+    IRule() = default;
+};
+
+// typealias like in Swift
+using RulePtr = shared_ptr<IRule>;
+
+} // namespace sentinel::engine
+
+#endif
